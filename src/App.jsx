5c7796888f174a6c82d9e2532a315b96e4c6233d
Shantanu <shantanu.kumar.singh26@gmail.com>
2026-07-25 12:16:41 +0530

    Responsive design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer';
 import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
@@ -150,6 +151,7 @@ const reset=()=>{
         e.target.innerHTML= '<img src="cross.svg" style="width:60px;" alt="" />'
         
         newarr[parseInt(e.target.id)]=1;
+        setturn(!turn);
         setarray(newarr)
         console.log(newarr)
       }
@@ -157,6 +159,7 @@ const reset=()=>{
         e.target.innerHTML= '<img src="circle.svg" style="width:60px;" alt="" />'
         newarr[parseInt(e.target.id)]=0;
         setarray(newarr);
+        setturn(!turn);
         console.log(newarr)
 
       }
@@ -181,21 +184,22 @@ theme="light"
    <Navbar/>
    
    <div className='mx-auto text-center font-[Ubuntu] text-xl mt-2'>Play with friends</div>
-   <div className="w-[80%] flex justify-between mx-auto mt-5">
-       <div className="box w-[45%] bg-black flex justify-between text-white font-[Orbitron]">
-          <div className='flex gap-3'><img src="person.svg" alt="" />
+   <div className="w-[80%] max-lg:flex-col max-lg:gap-4 flex justify-between mx-auto mt-5">
+       <div className="box w-full md:w-[45%] bg-black flex justify-between text-white font-[Orbitron]">
+          <div className='flex gap-3 w-full'><img src="person.svg" alt="" />
            <div className="flex flex-col justify-center"><p>Person 1</p><p>O Marker</p></div></div>
           <div className="flex flex-col bg-[#1d3fff] items-center px-3 py-2"><p className='text-2xl'>{player1}</p><p className='text-xs'>Points</p></div>
 
         </div>
-        <div className="box w-[45%] bg-black flex justify-between text-white font-[Orbitron]">
-          <div className='flex gap-3'><img src="person.svg" alt="" />
+        <div className="box w-full md:w-[45%] bg-black flex justify-between text-white font-[Orbitron]">
+          <div className='flex gap-3 w-full'><img src="person.svg" alt="" />
            <div className="flex flex-col justify-center"><p>Person 2</p><p>X Marker</p></div></div>
           <div className="flex flex-col bg-red-700 items-center px-3 py-2"><p className='text-2xl'>{player2}</p><p className='text-xs'>Points</p></div>
 
         </div>
    </div>
-   <div className="bg-black mt-4 w-[50%] mx-auto flex flex-col items-center">
+  {win? <div className='text-white px-3 py-1.5 mx-auto mt-3 cursor-default flex items-center gap-1 w-fit bg-green-800'>CONGRATULATIONS!</div>: <div className={`w-fit ${turn? "bg-red-700": "bg-[#1d3fff]"} text-white px-3 py-1.5 mx-auto mt-3 cursor-default flex items-center gap-1`}>Player {turn? "2": "1"} Turn <img className='down' src="down.svg" alt="" /></div>} 
+   <div className="bg-black mt-4 w-[90%] md:w-[50%] mx-auto flex flex-col items-center">
       <div className='flex justify-around w-full border-1 shadow shadow-[orange]'>
         <div id='0' className="h-[100px] w-[33.6%] justify-center flex items-center border border-white " onClick={e=> {handlemove(e)}}></div>
         <div id='1' className="h-[100px] w-[33.6%] justify-center flex items-center border border-white " onClick={e=> {handlemove(e)}}></div>
@@ -213,10 +217,10 @@ theme="light"
       </div>
    </div>
 
-    <div className="button flex flex-col gap-4 w-[50%] mx-auto mt-4 justify-center items-center">{win&&<button><a href="" className='bg-[#219b21] hover:bg-[#0c560c] text-white px-2 py-2 font-[Roboto] rounded-md'>Play Again</a></button> }
+    <div className="button pb-[60px] pb flex flex-col gap-4 w-[50%] mx-auto mt-4 justify-center items-center">{win&&<button><a href="" className='bg-[#219b21] hover:bg-[#0c560c] text-white px-2 py-2 font-[Roboto] rounded-md'>Play Again</a></button> }
 <button className='bg-red-600 text-white w-fit flex items-center px-2 font-[Roboto] hover:bg-[#bb0606] py-1 rounded-md cursor-pointer' onClick={reset}>Reset Points <img src="reset.svg" alt="" /></button>
 </div>
-
+<Footer/>
    </>
   )
 }
