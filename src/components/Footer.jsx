c63fa6e3b6ac9dda82eb6e9c72cba74614f51d98
Shantanu <shantanu.kumar.singh26@gmail.com>
2026-07-25 12:26:46 +0530

    Footer details updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='fixed bottom-0 w-full bg-black text-white h-[50px] flex gap-1 items-center justify-center font-[Ubuntu]'>
-        <p>Developed by </p><a href="" className='text-blue-600 hover:underline'>Shantanu</a>
+        <p>Developed by </p><a href="https://myportfolio-one-topaz-21.vercel.app/" target='_' className='text-blue-600 hover:underline'>Shantanu</a>
       
     </div>
   )
